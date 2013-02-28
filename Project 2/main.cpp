286a7b99ed289344ef8275aef8239485807271d2
Simon Gustafsson <simon@Simons-MacBook-Pro.local>
2013-02-28 13:12:18 -0800

    Initial Commit

---
+++ Project 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project 2
+//
+//  Created by Simon Gustafsson on 2/28/13.
+//  Copyright (c) 2013 Simon Gustafsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
