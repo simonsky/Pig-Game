38841b2fa45c0b640020e8411de945748855d8e6
simonsky <simon.j.gustafsson@gmail.com>
2013-03-07 09:10:08 -0800

    Pig Game

--- Project 2/main.cpp
+++ Project 2/main.cpp
@@ -10,9 +10,12 @@
 
 int main(int argc, const char * argv[])
 {
+    // Main game wall. Calls the function turn in order to play one turn for one player and checks if the score is over or equal to 100 and then sends both player score to the check_winner function,
 
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
 
+// This function simulates one turn for one player.  It uses the roll function to simulate the dice rols. Input player number and total player score, output new total score after the round.
+    // While loop used to let the players keep rolling for as many rolls they whishes.
